16bb506b3d27bfbc3c3ea43b262f88121bb1827d
RafalTalaskaa <rafixon220@wp.pl>
2022-01-31 11:30:43 +0100

    Caesardecryption added

--- TextCoder.c
+++ TextCoder.c
@@ -1,6 +1,61 @@
 #include <stdio.h> 
 int main(){
+    int mode = 1;
 
-printf("Hello World");
-return 0;
+    switch(mode){
+        case 1 : decryptCaesar();
+        break;
+
+    
+    }
+
+
+
+}
+
+
+void decryptCaesar(){
+
+    char input[100];
+    char temp;
+    int i, j, key;
+
+     printf("Podaj słowo do odszyforwania ");
+            scanf("%s", input);
+            printf("Podaj klucz: ");
+            scanf("%d", &key);
+            for (i = 0; input[i] != '\0'; ++i)
+            {
+                temp = input[i];
+
+                if (temp >= 'a' && temp <= 'z')
+                {
+
+                    temp = temp - key;
+
+                    if (temp < 'a')
+                    {
+
+                        temp = temp + 'z' - 'a' + 1;
+                    }
+
+                    input[i] = temp;
+                }
+                else if (temp >= 'A' && temp <= 'Z')
+                {
+
+                    temp = temp - key;
+
+                    if (temp < 'A')
+                    {
+
+                        temp = temp + 'Z' - 'A' + 1;
+                    }
+
+                    input[i] = temp;
+                }
+            }
+            printf("Odszyfrowana wiadomość to: %s", input);
+
+
 }
